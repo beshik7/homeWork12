8485a5de4624eb01d0654d8f9e3b8ebb64eb72c4
beshik7 <tommypdx7@gmail.com>
2023-03-15 09:45:57 -0700

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,16 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
         System.out.println(" Задача 1 ");
-        int year = 2021; // параметр проверяемого года
+        int year = 2021; // инициализировал параметр проверяемого года (чтобы добавить этот параметр в скобки к методу)
         checkLeapYear(year);
     }
 
-    public static void checkLeapYear(int year) {
+    public static void checkLeapYear(int year) {  // для облегчения чтения кода использовал параметр для метода в скобках
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год — високосный год");
         } else {
@@ -50,6 +51,24 @@ public class Main {
             } else {
                 System.out.println("Неверный тип операционной системы");
             }
+        }
+    }
+    public static void task3() {
+        System.out.println(" Задача 3 ");
+        int deliveryDistance = 95; // дистанция до клиента
+        int deliveryDays = calculateDeliveryDays(deliveryDistance); //присваиваю параметр для вызова метода со значением расстояния
+        System.out.println("Количество дней для доставки: " + deliveryDays);
+    }
+
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return 1; // доставка в пределах 20 км занимает сутки
+        } else if (deliveryDistance <= 60) {
+            return 2; // доставка в пределах от 20 км до 60 км добавляет еще один день доставки
+        } else if (deliveryDistance <= 100) {
+            return 3; // доставка в пределах от 60 км до 100 км добавляет еще одни сутки
+        } else {
+            return -1; // свыше 100 км доставки нет, возвращаем -1 для обозначения невозможности доставки
         }
     }
 }
